9b35660b642feb700725f17730db6fa11af6c91c
Nguyen Minh Chien <nmchien116@gmail.com>
2025-09-25 16:54:15 +0700

    feat: add login-page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Button } from "@/components/ui/button";
-
+import LoginPage from "./modules/login/LoginPage";
 function App() {
   return (
-    <div className="">
-      <Button>Click me</Button>
-    </div>
+    <>
+
+      <b className="mx-24 mb-9 w-2xl mt-9">Login form:</b>
+      <LoginPage>
+      </LoginPage>
+      <p className="mx-24 mb-9 w-2xl">
+        <b>Vì sao phải sử dụng key trong list rendering</b>
+        <br />
+        Thuộc tính key trong lệnh for (hoặc v-for trong Vue, map trong React) là
+        bắt buộc vì nó giúp framework (như React, Vue) nhận dạng, theo dõi và
+        tối ưu hóa quá trình cập nhật các phần tử trong một danh sách. Nếu không
+        có key, framework sẽ dựa vào thứ tự của các phần tử, dẫn đến hiệu suất
+        kém và lỗi hiển thị khi danh sách thay đổi, chẳng hạn như khi sắp xếp
+        hoặc xóa phần tử.{" "}
+      </p>
+    </>
   );
 }
 
--- src/modules/login/LoginPage.tsx
+++ src/modules/login/LoginPage.tsx
@@ -1,8 +1,10 @@
-export default function LoginPage(){
-    const title1 = "title 1";
-    const title2 = "title 2";
-
-    return ( <div>
-       
-    </div>)
+import { LoginForm } from "@/components/login-form"
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
